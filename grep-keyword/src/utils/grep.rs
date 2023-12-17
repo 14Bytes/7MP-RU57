46419ed34e4a2351e84e1695de8cb6ef4f73bc5a
J1n H4ng <jinhang@mail.14bytes.com>
2023-12-17 19:42:26 +0800

    feat(grep_keyword): grep 功能模块编写
    Signed-off-by: J1n H4ng <jinhang@mail.14bytes.com>

--- grep-keyword/src/utils/grep.rs
+++ grep-keyword/src/utils/grep.rs
@@ -1,8 +1,29 @@
-pub fn grep() {
-    println!("Hello, Public!");
-    private();
-}
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-fn private() {
-    println!("Hello, Private!");
+fn find_keyword(log_file_path: &str, keyword: &str) -> io::Result<Vec<String>> {
+    let file = File::open(log_file_path)?;
+    let reader = BufReader::new(file);
+
+    let mut lines = Vec::new();
+    let mut found = false;
+
+    for line in reader.lines() {
+        let line = line?;
+
+        if line.contains(keyword) {
+            found = true;
+        }
+
+        lines.push(line);
+
+        if lines.len() > 40 {
+            lines.remove(0);
+        }
+
+        if found && lines.len() == 40 {
+            return Ok(lines);
+        }
+    }
+    Ok(lines)
 }
